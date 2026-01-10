2111b174bdd887989902c16710173aff1cd8520c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-10 11:44:34 +0530

    PHP Create MySQLTables

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 $servername="localhost";
 $username="root";
 $password="";
+$dbname ="myDB";
 
 //Create connectionection
-$connection=new mysqli($servername,$username,$password);
+$connection=new mysqli($servername,$username,$password, $dbname);
 
 	if($connection->connect_error){
 		die("connectionection failed: ".$connection->connect_error);
 	}
-	//create database
-	$sql="CREATE DATABASE myDB";
+	//sql to create table
+	$sql="CREATE TABLE MyGuests(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, email VARCHAR(50))";
+
 	if($connection -> query($sql) === TRUE){
-		echo "Database created";
+		echo "Table MyGuests created successfully";
 	}else{
-		echo "Error creating database: ".$connection->error;
+		echo "Error creating table: ".$connection->error;
 	}
 
 	$connection->close();
