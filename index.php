b59df89a73a2b88b256300208431e45c5b4b1516
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-10 12:12:47 +0530

    PHP Insert multiple Data records Into MySQL

--- index.php
+++ index.php
@@ -10,10 +10,12 @@ $connection=new mysqli($servername,$username,$password, $dbname);
 	if($connection->connect_error){
 		die("connectionection failed: ".$connection->connect_error);
 	}
-	//insert data into table
-	$sql="INSERT INTO MyGuests(firstname,lastname,email)VALUES('John','Cena','john@example.com')";
+	//insert multiple Data records Into MySQL
+	$sql="INSERT INTO MyGuests(firstname,lastname,email)VALUES('John','Cena','john@example.com');";
+	$sql .="INSERT INTO MyGuests(firstname,lastname,email)VALUES('Mary','Moe','mary@example.com');";
+	$sql .="INSERT INTO MyGuests(firstname,lastname,email)VALUES('Jullie','Dooley','julie@example.com')";
 
-	if($connection -> query($sql) === TRUE){
+	if($connection -> multi_query($sql) === TRUE){
 		echo "New record created successfully";
 	}else{
 		echo "Error: " . $sql . "<br>".$connection->error;
