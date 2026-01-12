943a3fef4891564434fbf25f4fbac6eb6eb1004a
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-12 11:06:18 +0530

    PHP script to display all students from the database:

---
+++ student_management_system/display.php
@@ -0,0 +1,14 @@
+<?php
+include "db.php";
+$sql='SELECT * FROM students';
+$result=$conn->query($sql);
+
+if($result->num_rows>0){
+    while($row =$result->fetch_assoc()){
+        echo 'ID:'.$row['id'].' -Name:'.$row['name'].' -Email:'.$row['email'].' -Age:'.$row['age'].' -Address:'.$row['address']. '<br>';
+    }
+}else{
+    echo '0 results';
+}
+
+?>
